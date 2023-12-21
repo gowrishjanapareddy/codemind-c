ca05ec4e16837fd8d1e3a050810be37439e985a2
gowrishjanapareddy <147791808+gowrishjanapareddy@users.noreply.github.com>
2023-12-21 10:00:08 +0530

    #include<stdio.h> int main() {     int a,i,j,k,l;     scanf("%d",&a);     for(i=a;i>=1;i--)     {         for(j=1;j<=i;j++)         {             printf("%d",j);         }         for(k=a-i;k>=1;k--)         {             printf("*");         }         printf(" ");     } }

---
+++ Basic_Pattern_5.4.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int a,i,j,k,l;
+    scanf("%d",&a);
+    for(i=a;i>=1;i--)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("%d",j);
+        }
+        for(k=a-i;k>=1;k--)
+        {
+            printf("*");
+        }
+        printf("
+");
+    }
+}
